0deed3f1250a643dbded5006a33028c981b18f6b
Visual Laser 10 New <visualaser10new@gmail.com>
2023-10-06 20:49:19 +0200

    defined real funcs

--- apitr.py
+++ apitr.py
@@ -6,6 +6,9 @@ class api:
 		#Wed Oct 04 2023 17:21:03 GMT+0200 (Ora legale dell’Europa centrale)
 		return datetime.now().strftime("%a %b %d %Y %H:%M:%S GMT+0200 (Ora legale dell’Europa centrale)")
 	
+	def __request(self, uri):
+		pass
+
 	def __getUri(self, idStazione):
 		date = self.__getDate()
 		partenza ='http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/partenze/'+idStazione+'/'+date
@@ -18,21 +21,31 @@ class api:
 	
 		#http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/andamentoTreno/S00245/Wed Oct 04 2023 17:21:03 GMT+0200 (Ora legale dell’Europa centrale)
 		
-	def getInfoStazione(self, idStazione):
+	def getInfoCirc(self):
+		# GET /resteasy/viaggiatreno/infomobilitaTicker/
 		pass
 
 	def getPartenze(self, idStazione):
+		# GET /resteasy/viaggiatreno/partenze/{codiceStazione}/{orario}
 		pass
 
 	def getArrivi(self, idStazione):
+		# GET /resteasy/viaggiatreno/arrivi/{codiceStazione}/{orario}
 		pass
 
 	def getAndamento(self, idTreno):
+		# GET /resteasy/viaggiatreno/andamentoTreno/{codOrigine}/{numeroTreno}/{dataPartenza}
 		pass
 
 	def soluzioneViaggio(self, partenza, arrivo, data, ora):
+		# GET /resteasy/viaggiatreno/soluzioniViaggioNew/{codLocOrig}/{codLocDest}/{date}
+		pass
+
+	def searchStazione(self, nomeStazione):
+		# GET /resteasy/viaggiatreno/cercaStazione/{text}
 		pass
 
 	def getStazioniByRegione(self, regione):
+		# GET /resteasy/viaggiatreno/elencoStazioni/{regione}
 		pass
 
