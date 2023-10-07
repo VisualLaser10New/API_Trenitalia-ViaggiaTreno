5f139df33c1ae2ccc02f162c6f5fdaeec9d2b153
Visual Laser 10 New <visualaser10new@gmail.com>
2023-10-07 10:56:19 +0200

    write methods, complete version 0.1.0

--- README.md
+++ README.md
@@ -2,12 +2,51 @@
 ## Introduzione
 Work in progress...
 
- Api (non ufficiali) in python del sito viaggiatreno.it. 
+ API (non ufficiali) in python del sito viaggiatreno.it. 
 Queste api utilizzano dati pubblici forniti dal server web http://www.viaggiatreno.it/infomobilita/, il solo scopo di queste api è fornire supporto ai programmatori di python per la realizzazione di applicazioni inerenti alla mobilità ferroviaria italiana, non ci assumme responsabilità per applicazioni di terzi riguardanti l'utilizzo improprio del codice e dei dati pubblici.
 
 ## Funzionamento
  Queste API non eseguono scraping nelle varie pagine del sito; tali infatti effettuano richieste dati al server di viaggiatreno.it attraverso il metodo GET del protocollo HTTP, il server interrogato correttamente restituisce dati JSON e/o text-plain.
 Lo studio del funzionamento del server è stato realizzato grazie al codice pubblico JS, JQuery e AJAX che il sito originale presenta.
 
-Le API in python sono tuttora in uno stato primordiale: vengono restituiti solamente dati sottoforma di dizionari (dict).
+Le API in python sono tuttora in uno stato primordiale: vengono restituiti solamente dati sottoforma di dizionari (dict) o json.
 
+## Utilizzo
+La classe principale delle api è *apitr*, la dichiarazione del costruttore prevede un metodo per il quale, impostato a true i dati *Json* vengono automaticamente convertiti in *dict*.
+
+### Elenco dei Metodi
+- ```getInfoMob()```
+Ottiene le informazioni aggiornate sulla mobilità ferroviaria
+
+- ```getPartenze(idStazione, dataora)```
+Passando il _codice della stazione_ e il datetime della data e ora attuale, ottiene le informazioni sui treni in partenza dalla stazione specificata
+
+- ```getArrivi(idStazione, dataora)```
+Passando il _codice della stazione_ e il datetime della data e ora attuale, ottiene le informazioni sui treni in arrivo alla stazione specificata
+
+- ```getAndamento(idStazioneOrigine, idStazioneArrivo, dataoraPartenza)```
+Passando sia il _codice della stazione_ di origine sia quello di arrivo  e datetime della data e ora di partenza del treno, ottiene le informazioni sulla percorrenza
+
+- ```getIndicazioniViaggio(idStazioneOrigine, idStazioneArrivo, dataoraAttuale)```
+Ottiene le tratte possibili per pianificare un viaggio data una stazione di origine e una di arrivo
+
+- ```searchStazione(nomeStazione)```
+Ottiene i dati delle stazioni i quali nomi iniziano con il testo passato
+
+- ```getCodStazione(nomeStazione)```
+Restituisce il codice totale della stazione avente come nome completo il testo passato
+
+Esempio di utilizzo delle API in python:
+```python
+from apitr import apitr
+
+a = apitr(decodeJson= True)
+
+#ottenimento dei treni in arrivo alla stazione S01700
+arr = a.getArrivi('S01700', datetime.now())
+
+#ottenimento dei dati di un treno in arrivo alla stazione sopracitata
+print(a.getAndamento(arr[10]['codOrigine'], 
+	str(arr[10]['numeroTreno']), 
+	a.timestamp2datetime(arr[10]['dataPartenzaTreno']))))
+```
--- apitr.py
+++ apitr.py
@@ -1,51 +1,111 @@
 from datetime import datetime
+import requests
 
+'''
+	API Trenitalia
+	Version: 0.1.0
+	Visual Laser 10 New - 10/2023
+'''
 
-class api:
-	def __getDate(self):
-		#Wed Oct 04 2023 17:21:03 GMT+0200 (Ora legale dell’Europa centrale)
-		return datetime.now().strftime("%a %b %d %Y %H:%M:%S GMT+0200 (Ora legale dell’Europa centrale)")
+class apitr:
+	__decodeJson = True
+	def __init__(self, decodeJson:bool = True):
+		# decodeJson: True = return dict, False = return text/plain
+		self.__decodeJson = decodeJson
+
+	__uris = {
+		'InfoMob': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/infomobilitaTicker/',
+		'partenze': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/partenze/',
+		'arrivi': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/arrivi/',
+		'andamento': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/andamentoTreno/',
+		'indicazioniViaggio': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/soluzioniViaggioNew/',
+		'searchStazione': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/cercaStazione/',
+		'StazioniByRegione': 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/elencoStazioni/'
+	}
+
+	__datetimeFormat = {
+		'partenze':'%a %b %d %Y %H:%M:%S GMT+0200 (Ora legale dell’Europa centrale)',
+		'arrivi':'%a %b %d %Y %H:%M:%S GMT+0200 (Ora legale dell’Europa centrale)',
+		'andamento':'timestamp',
+		'indicazioniViaggio':'%Y-%m-%dT%H:%M:%S'	#YYYY-MM-DDTHH:MM:SS
+	}
+
+	def __dateTime2Str(self,date: datetime, format:str):
+		if (format == 'timestamp'):
+			return str(int(date.timestamp()))+'000'
+		else:
+			return date.strftime(format)
 	
 	def __request(self, uri):
-		pass
+		x = requests.get(uri, headers={'Accept-Charset': 'utf-8'})
+		#set to use utf-8
+		if (x.status_code == 200):
+			try:
+				if (self.__decodeJson):
+					return x.json()
+				else:
+					return x.text
+			except:
+				return x.text
+		else:
+			return None
 
-	def __getUri(self, idStazione):
-		date = self.__getDate()
-		partenza ='http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/partenze/'+idStazione+'/'+date
-		arrivo = 'http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/arrivi/'+idStazione+'/'+date
+	def __minimizeCodStazione(self, codStazione:str):
+		codStazione = codStazione[1:]
+		return str(int(codStazione))
 
-		andamento = 'http://www.viaggiatreno.it/viaggiatrenonew/resteasy/viaggiatreno/andamentoTreno/S00245/100/2017-10-04'
 
-		
-	
-	
-		#http://www.viaggiatreno.it/infomobilita/resteasy/viaggiatreno/andamentoTreno/S00245/Wed Oct 04 2023 17:21:03 GMT+0200 (Ora legale dell’Europa centrale)
-		
-	def getInfoCirc(self):
+	####### PUBLIC METHODS #######
+	def getInfoMob(self):
 		# GET /resteasy/viaggiatreno/infomobilitaTicker/
-		pass
+		return self.__request(self.__uris['InfoMob'])
 
-	def getPartenze(self, idStazione):
+	def getPartenze(self, idStazione:str, dataora: datetime):
 		# GET /resteasy/viaggiatreno/partenze/{codiceStazione}/{orario}
-		pass
+		return self.__request(self.__uris['partenze'] + idStazione 
+								+ '/' + self.__dateTime2Str(dataora, self.__datetimeFormat['partenze']))
 
-	def getArrivi(self, idStazione):
+	def getArrivi(self, idStazione:str, dataora: datetime):
 		# GET /resteasy/viaggiatreno/arrivi/{codiceStazione}/{orario}
-		pass
+		return self.__request(self.__uris['arrivi'] + idStazione 
+								+ '/' + self.__dateTime2Str(dataora, self.__datetimeFormat['arrivi']))
 
-	def getAndamento(self, idTreno):
+	def getAndamento(self, idStazioneOrigine:str, idTreno:str, dataoraPartenza: datetime):
 		# GET /resteasy/viaggiatreno/andamentoTreno/{codOrigine}/{numeroTreno}/{dataPartenza}
-		pass
+		return self.__request(self.__uris['andamento'] + idStazioneOrigine + '/' + idTreno 
+								+ '/' + self.__dateTime2Str(dataoraPartenza, self.__datetimeFormat['andamento']))
 
-	def soluzioneViaggio(self, partenza, arrivo, data, ora):
+	def getIndicazioniViaggio(self, idStazioneOrigine: str, idStazioneArrivo:str, dataora: datetime):
 		# GET /resteasy/viaggiatreno/soluzioniViaggioNew/{codLocOrig}/{codLocDest}/{date}
-		pass
+		idStazioneArrivo = self.__minimizeCodStazione(idStazioneArrivo)
+		idStazioneOrigine = self.__minimizeCodStazione(idStazioneOrigine)
 
-	def searchStazione(self, nomeStazione):
+		return self.__request(self.__uris['indicazioniViaggio'] + idStazioneOrigine + '/' + idStazioneArrivo
+								+ '/' + self.__dateTime2Str(dataora, self.__datetimeFormat['indicazioniViaggio']))
+
+	def searchStazione(self, nomeStazione:str):
 		# GET /resteasy/viaggiatreno/cercaStazione/{text}
-		pass
+		return self.__request(self.__uris['searchStazione'] + nomeStazione)
 
-	def getStazioniByRegione(self, regione):
+	def getStazioniByRegione(self, codRegione:str):
 		# GET /resteasy/viaggiatreno/elencoStazioni/{regione}
-		pass
+		return self.__request(self.__uris['StazioniByRegione'] + codRegione)
 
+	def getCodStazione(self, nomeStazione:str):
+		# return codStazione from nomeStazione
+		stazioni = self.searchStazione(nomeStazione)
+		if (stazioni != None):
+			try:
+				return [stazione['id'] for stazione in stazioni if stazione['nomeLungo'].lower() == nomeStazione.lower()][0]
+			except:
+				return None
+		else:
+			return None
+		
+	####### TOOLS METHODS #######
+	def timestamp2datetime(self, timestamp):
+		# convert timestamp with || without millisec to datetime
+		try:
+			return datetime.fromtimestamp(int(timestamp))
+		except:
+			return datetime.fromtimestamp(int(timestamp)/1000)
